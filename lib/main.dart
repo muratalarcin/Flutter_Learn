7645fee1140eedfdd75cc6173de90586fc2bae3e
Murat Alarcin <murattrbl@icloud.com>
2024-01-15 00:27:09 +0300

    Revert "list_tile_learn"
    This reverts commit 58a7c2164e9b11bcd389e2572c627c0e02c278d0.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_first/101/list_tile_learn.dart';
+import 'package:flutter_application_first/101/indicator_learn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       //   backgroundColor: Colors.red,
       //   elevation: 0,
       // )),
-      home: const ListTileLearn(),
+      home: const IndicatorLearn(),
     );
   }
 }
